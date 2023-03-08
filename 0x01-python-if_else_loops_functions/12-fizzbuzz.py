a2a8b202c798384d2e005007be738d4761d25335
WanderHermit <mysttrevor16@gmail.com>
2023-03-08 04:06:58 -0800

    fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(101):
+    for i in range(1, 101):
         if i % 3 == 0:
             print('Fizz')
         if i % 5 == 0:
             print('Buzz')
         if i % 15 == 0:
             print('FizzBuzz')
+        else:
+            print(i, end= ", ")
