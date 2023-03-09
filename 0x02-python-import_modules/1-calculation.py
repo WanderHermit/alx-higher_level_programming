9a82fc4a51b1591e2399be192ea2ec103b67534e
WanderHermit <mysttrevor16@gmail.com>
2023-03-09 03:04:33 -0800

    calculator

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,7 +2,7 @@
 
 if __name__ == "__main__":
     """Perform calculations"""
-    from calculator_1.py import calculations
+    from calculator_1 import add, sub, mul, div
 
     a = 10
     b = 5
@@ -11,10 +11,10 @@ if __name__ == "__main__":
     print("{} + {} = {}".format(a, b, add(a, b)))
 
     # Subtraction
-    print("{} - {} = {}".format(a, b, subtract(a, b))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
 
     # Multiplication
-    print("{} * {} = {}".format(a, b, multiply(a, b))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
 
     # Division
-    print("{} / {} = {}".format(a, b, divide(a, b))
+    print("{} / {} = {}".format(a, b, div(a, b)))
