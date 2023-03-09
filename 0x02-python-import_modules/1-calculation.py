8a386219fa87bc84e78294d3d37c7254ac38a482
WanderHermit <mysttrevor16@gmail.com>
2023-03-09 02:59:24 -0800

    calculations

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Perform calculations"""
+    from calculator_1.py import calculations
+
+    a = 10
+    b = 5
+
+    # Addition
+    print("{} + {} = {}".format(a, b, add(a, b)))
+
+    # Subtraction
+    print("{} - {} = {}".format(a, b, subtract(a, b))
+
+    # Multiplication
+    print("{} * {} = {}".format(a, b, multiply(a, b))
+
+    # Division
+    print("{} / {} = {}".format(a, b, divide(a, b))
