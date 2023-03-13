c5fbc18e7c50c839a48e81f8eec7211a47362dc1
WanderHermit <mysttrevor16@gmail.com>
2023-03-13 03:29:31 -0700

    multiple returns

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!usr/bin/python3
+def multiple_returns(sentence):
+    length = len(str)
+    First_character = str[0]
+    if length == 0:
+        print('First character = {}'.format(None))
+    else:
+        print("Length: {:d} - First character: {}".format(length, First_character))
