6e921b79ecde94afe65e541c450e5a9461dde16f
WanderHermit <mysttrevor16@gmail.com>
2023-03-13 05:40:30 -0700

    dividible by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    for t in range(len(my_list))
-    if t / 2 % 0:
-        return 'True'
-    else:
-        return 'False'
+    for t in range(len(my_list)):
+        if t / 2 % 0:
+            return 'True'
+        else:
+            return 'False'
