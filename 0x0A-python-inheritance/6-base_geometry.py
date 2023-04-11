a24e018cf6ed65aabc49180b2ba886f19c183083
WanderHermit <mysttrevor16@gmail.com>
2023-04-11 13:35:52 +0000

    Tasks

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define a class BaseGeometry."""
+
+
+class BaseGeometry:
+	"""Represent the class BaseGeometry."""
+\
+	def area(self):
+		"""When Not implemented."""
+		raise Exception("area() is not implemented")
