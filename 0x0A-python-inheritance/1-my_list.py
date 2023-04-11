537c816c4acf1414278554f99e65c8337a293cf3
WanderHermit <mysttrevor16@gmail.com>
2023-04-11 10:06:27 +0000

    MyList

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Class that inherits from another class."""
+
+
+def print_sorted(list):
+    """Public instance method"""
+    class list:
+        """Class from which MyList inherits"""
+        pass
+
+    class MyList:
+        """Child class of the parent object List."""
+        pass
