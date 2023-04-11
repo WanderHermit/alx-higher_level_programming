69612d0c9b3940470eb9e468c85407a4e4ce00d3
WanderHermit <mysttrevor16@gmail.com>
2023-04-11 12:15:05 +0000

    Task 4 - inherited class

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Check if object is instance of an inherited class."""
+
+
+def inherits_from(obj, a_class):
+	"""Function to check the code.
+
+	Args:
+		obj (any): The object to check.
+		a_class (any): The class to check with.
+
+	Returns:
+		if obj is an instance of an inherited class - True.
+		Otherwise - False.
+	"""
+	if isinstance:
+		return True
+	return False
