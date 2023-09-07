b75ddf5cb4e77e5560305b62200e9096487f2ffd
WanderHermit <mysttrevor16@gmail.com>
2023-09-07 00:07:34 -0700

    Correction

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
+"""Makes the code executable."""
+
 
 def add_integer(a, b=98):
-    """Add integers."""
+    """Add integers.
 
-    if not isisntance(a, int) or (a, float):
+    Raise TypeError if input is not a float or an int
+    """
+
+    if (not isisntance(a, int) or (a, float)):
         raise TypeError("a must be an integer")
-    elif not isisntance(b, int) or (b, float):
+    elif (not isisntance(b, int) or (b, float)):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
