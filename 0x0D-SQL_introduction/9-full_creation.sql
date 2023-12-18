12281ba4ef1c39bde5e85ff8f859cead315fd65f
WanderHermit <mysttrevor16@gmail.com>
2023-12-18 05:46:11 -0800

    New table with multiple rows

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,11 @@
+-- Create new table and add multiple rows.
+CREATE TABLE second_table(
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
+INSERT (id, name, score) VALUES (1, 'John', 10);
+INSERT (id, name, score) VALUES (2, 'Alex', 3);
+INSERT (id, name, score) VALUES (3, 'Bob', 14);
+INSERT (id, name, score) VALUES (4, 'George', 8);
+
